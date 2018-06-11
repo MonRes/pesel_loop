c322e66bcd62155bf445788e7414b6fc9f6b3c00
Restek <Restek87@gmail.com>
2018-06-11 17:34:35 +0200

    upgrade commit

--- pesel_tools.py
+++ pesel_tools.py
@@ -1,15 +1,16 @@
-""" Module for validation and for retrieving data from pesel number"""
+"""Module for validation and for retrieving data from pesel"""
 
 def validate(pesel):
 
-    """ Validation of the pesel number
-        (if the characters are digits,
-        number of digits, checksum, correctness of the date of birth)
-        Args:
-            pesel: pesel number
-        Return:
-            information about corectness of pesel number
-        """
+    """Validation of the pesel number
+    (if the cahracters are digits,
+    number of digits, cheksum,
+    correctness of the date of birth
+    Args:
+        pesel: pesel number
+    Return:
+        information about correctness of the pesel number"""
+
 
     MONTH_LENGTH = (31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
     year = pesel[0:2]
@@ -27,43 +28,45 @@ def validate(pesel):
     elif pesel[2] == '8' or pesel[2] == '9':
         new_year = '18' + year
 
+    if pesel.isdigit() and len(pesel) == 11:
+        multip = 9 * (int(pesel[0]) + int(pesel[4]) + int(pesel[8])) + 7 * (int(pesel[1]) + int(pesel[5]) +
+                        int(pesel[9])) + 3 * (int(pesel[6]) + int(pesel[2])) + int(pesel[3]) + int(pesel[7])
 
-    if pesel.isdigit() and len(pesel) == 11: #sprawdzamy czy podane znaki są cyframi i czy jest ich 11
-
-        multip = 9 * int(pesel[0]) + 7 * int(pesel[1]) + 3 * int(pesel[2]) + int(pesel[3]) + 9 * int(pesel[4]) + \
-                 7 * int(pesel[5]) + 3 * int(pesel[6]) + int(pesel[7]) + 9 * int(pesel[8]) + 7 * int(pesel[9]) #liczenie sumy kontrolnej
-
-        if multip % 10 == int(pesel[10]):
-            if month != '02' and month != '82' and month != '22' and month != '42' and month !='62':
+        month_table = ['02', '82', '22', '42', '62']
+        if multip %10 == int(pesel[10]):
+            if month not in month_table:
                 if int(day) <= MONTH_LENGTH[int(month)-1]:
                     return True
-            elif (int(new_year) % 4 == 0 and int(new_year) % 100 != 0) or int(new_year) % 400 == 0:
-                if int(day) <= 29:
-                    return True
-                else:
+                elif (int(new_year) %4 == 0 and int(new_year) %100 != 0) or int(new_year) %400 == 0:
+                    if int(day) <= 29:
+                        return True
                     return False
-            else:
-                if int(day) <= 28:
-                    return True
                 else:
+                    if int(day) <= 28:
+                        return True
                     return False
+                return False
             return False
         return False
     return False
 
-def extract_personal_data(pesel):
 
-    """ Extract personal data from pesel number
-        (birth date and sex )
-        Args:
-            pesel: pesel number
-        Return:
-            date_birth, sex
-        """
+def extract_personal_data(pesel):
+    """Extract personal data from pesel number
+    (birth date and sex)
+    Args:
+        pesel: pesel number
+    Returns:
+        date_birth, sex"""
+
+
+    if validate(pesel) == False:
+        raise ValueError ('Podany PESEL jest nieprawidłowy')
 
     year = pesel[0:2]
     month = pesel[2:4]
     day = pesel[4:6]
+    new_year = ''
 
     if pesel[2] == '0' or pesel[2] == '1':
         new_year = '19' + year
@@ -76,6 +79,7 @@ def extract_personal_data(pesel):
     elif pesel[2] == '8' or pesel[2] == '9':
         new_year = '18' + year
 
+
     if pesel[2] == '8' or pesel[2] == '2' or pesel[2] == '4' or pesel[2] == '6':
         new_month = '0' + pesel[3]
     elif pesel[2] == '9' or pesel[2] == '3' or pesel[2] == '5' or pesel[2] == '7':
@@ -83,12 +87,19 @@ def extract_personal_data(pesel):
     else:
         new_month = month
 
-    print('Data urodzenia: ', new_year, '/', new_month, '/', day)
+    all_data = '{} / {} / {}'.format(day, new_month, new_year)
 
-    sex = ['kobieta', 'mężczyzna']
-    if int(pesel[9]) % 2 == 0: #drukowanie płci
-        print('Płeć: ', sex[0])
+
+    sex = ['woman', 'man']
+    if int(pesel[9]) %2 == 0:
+        sex_ = sex[0]
     else:
-        print('Płeć: ', sex[1])
+        sex_ = sex[1]
 
+    pesel_info = {}
+
+    pesel_info['Bitrh Date: '] = all_data
+    pesel_info['Sex: '] = sex_
+
+    return pesel_info
 
--- pesel_ui.py
+++ pesel_ui.py
@@ -1,14 +1,5 @@
 import pesel_tools
 
-
-pesel = str(input('Witaj. Podaj Twój numer PESEL: '))
-
-isValidate = False
-while isValidate == False:
-    try:
-        isValidate = pesel_tools.validate(pesel)
-        isValidate = str(input(pesel))
-    except ValueError:
-        print('Błędny PESEL. Spróbuj jeszcze raz: ')
+pesel = input('Please, give the PESEL number: ')
 
 print(pesel_tools.extract_personal_data(pesel))
